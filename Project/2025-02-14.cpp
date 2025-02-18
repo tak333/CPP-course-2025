9278e4303628f9d37fc1c5db33af1f6289f7ca0d
fursova-au <fursova.a.u@gmail.com>
2025-02-19 01:03:34 +0900

    Add project files.

---
+++ Project/2025-02-14.cpp
@@ -0,0 +1,22 @@
+/*
+
+//#include <cstdio> //C-style
+
+#include <iostream>
+
+
+int main()
+{
+
+	//printf("Hello, world!"); //C-lib
+
+	//std::cout << "Hello, world!" << std::endl;
+
+	std::cout << "Hello\n" << 5 << 11 << '\a';
+
+	//std::cout << min(4, 5);
+
+	return 0;
+}
+
+*/
---
+++ Project/2025-02-15.cpp
@@ -0,0 +1,64 @@
+/*
+
+#include <iostream>
+
+int main()
+{
+	int a = -5;
+	unsigned b = 3;
+
+	std::cout << a + b << std::endl;
+
+	unsigned c = -1;
+
+	std::cout << c << std::endl;
+
+	int d = 2'147'483'648;
+
+	std::cout << d << std::endl;
+
+	return 0;
+}
+
+*/
+
+/*
+//Задание 1.
+
+#include <iostream>
+
+int main()
+{
+	int n;
+	std::cin >> n;
+
+	//allMinutes
+	//all_minutes
+
+	//const int MIN_PER_DAY = 24 * 60;
+	//n = n % MIN_PER_DAY;
+	//int hours = n / 60;
+
+	int hours = (n / 60) % 24;
+	int minutes = n % 60;
+
+	std::cout << hours / 10 << hours % 10 << ":" << minutes / 10 << minutes % 10 << std::endl;
+
+	return 0;
+}
+*/
+
+#include <iostream>
+
+int main()
+{
+	int a, b, c, d;
+	std::cin >> a >> b >> c >> d;
+
+	int sum = a + b + c + d;
+	float average = static_cast<float>(sum) / 4;
+
+	std::cout << average << std::endl;
+
+	return 0;
+}
