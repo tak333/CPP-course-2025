a3d41c81ef3c5943c095d934ea96b59a4abc8d67
fursova-au <fursova.a.u@gmail.com>
2025-04-04 17:49:56 +0300

    Код от занятия 4 апреля.

--- Project/2025-04-01.cpp
+++ Project/2025-04-01.cpp
@@ -1,111 +1,111 @@
-/*
-
-#include <iostream>
-
-struct TwoInt
-{
-	int first;
-	int second;
-};
-
-////передача по значению
-//void swap(int x, int y) {
+///*
+//
+//#include <iostream>
+//
+//struct TwoInt
+//{
+//	int first;
+//	int second;
+//};
+//
+//////передача по значению
+////void swap(int x, int y) {
+////	int buffer = x;
+////	x = y;
+////	y = buffer;
+////}
+//
+////передача по значению, возврат структуры
+////TwoInt swap(int x, int y) {
+////	TwoInt result;
+////
+////	result.first = y;
+////	result.second = x;
+////
+////	return result;
+////}
+//
+////передача по указателю
+//void swap(int* px, int* py) {
+//	int buffer = *px;
+//	*px = *py;
+//	*py = buffer;
+//}
+//
+////передача по указателю
+//void swap(int& x, int& y) {
 //	int buffer = x;
 //	x = y;
 //	y = buffer;
 //}
-
-//передача по значению, возврат структуры
-//TwoInt swap(int x, int y) {
-//	TwoInt result;
 //
-//	result.first = y;
-//	result.second = x;
-//
-//	return result;
+//void f(int arr[100]) {
+//	std::cout << typeid(arr).name() << std::endl;
 //}
-
-//передача по указателю
-void swap(int* px, int* py) {
-	int buffer = *px;
-	*px = *py;
-	*py = buffer;
-}
-
-//передача по указателю
-void swap(int& x, int& y) {
-	int buffer = x;
-	x = y;
-	y = buffer;
-}
-
-void f(int arr[100]) {
-	std::cout << typeid(arr).name() << std::endl;
-}
-
-int main()
-{
-	int a, b;
-
-	std::cin >> a >> b;
-
-	//swap(a, b); //doesn't work
-
-	//TwoInt changed = swap(a, b);
-	//a = changed.first;
-	//b = changed.second;
-
-	swap(&a, &b);
-	swap(a, b);
-
-	std::cout << a << " " << b << std::endl;
-
-	int arr[100] = { 1,2,3,4,5,6,70 };
-
-	std::cout << typeid(arr).name() << std::endl;
-
-	f(arr);
-
-
-	std::cout << arr[5] << std::endl;
-	std::cout << *(arr + 5) << std::endl;
-	std::cout << *(5 + arr) << std::endl;
-	std::cout << 5[arr] << std::endl;
-
-	return 0;
-}
-
-
-*/
-
-
-#include <iostream>
-
-int main()
-{
-	int arr[10] = { 1,2,5,7,9,4 };
-
-	//проход с индексацией
-	for (int i = 0; i < 10; ++i)
-	{
-		std::cout << arr[i] << " ";
-	}
-	std::cout << std::endl;
-
-	//арифметика указателей на индексах
-	for (int i = 0; i < 10; ++i)
-	{
-		std::cout << *(arr + i) << " ";
-	}
-	std::cout << std::endl;
-
-	//арифметика указателей в "чистом виде"
-	int* end = arr + 10;
-	for (int* ptr = arr; ptr < end; ++ptr)
-	{
-		std::cout << *ptr << " ";
-	}
-	std::cout << std::endl;
-
-	return 0;
-}
+//
+//int main()
+//{
+//	int a, b;
+//
+//	std::cin >> a >> b;
+//
+//	//swap(a, b); //doesn't work
+//
+//	//TwoInt changed = swap(a, b);
+//	//a = changed.first;
+//	//b = changed.second;
+//
+//	swap(&a, &b);
+//	swap(a, b);
+//
+//	std::cout << a << " " << b << std::endl;
+//
+//	int arr[100] = { 1,2,3,4,5,6,70 };
+//
+//	std::cout << typeid(arr).name() << std::endl;
+//
+//	f(arr);
+//
+//
+//	std::cout << arr[5] << std::endl;
+//	std::cout << *(arr + 5) << std::endl;
+//	std::cout << *(5 + arr) << std::endl;
+//	std::cout << 5[arr] << std::endl;
+//
+//	return 0;
+//}
+//
+//
+//*/
+//
+//
+//#include <iostream>
+//
+//int main()
+//{
+//	int arr[10] = { 1,2,5,7,9,4 };
+//
+//	//проход с индексацией
+//	for (int i = 0; i < 10; ++i)
+//	{
+//		std::cout << arr[i] << " ";
+//	}
+//	std::cout << std::endl;
+//
+//	//арифметика указателей на индексах
+//	for (int i = 0; i < 10; ++i)
+//	{
+//		std::cout << *(arr + i) << " ";
+//	}
+//	std::cout << std::endl;
+//
+//	//арифметика указателей в "чистом виде"
+//	int* end = arr + 10;
+//	for (int* ptr = arr; ptr < end; ++ptr)
+//	{
+//		std::cout << *ptr << " ";
+//	}
+//	std::cout << std::endl;
+//
+//	return 0;
+//}
---
+++ Project/2025-04-04.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+//pointer to 0st element
+void f(int arr[3])
+{
+	std::cout << typeid(arr).name() << std::endl;
+}
+
+int main()
+{
+	int arr[3] = { 1, 4, 8 };
+
+	//std::cout << arr[1] << std::endl;
+	
+	std::cout << "TYPE OF ARR:" << std::endl;
+	std::cout << typeid(arr).name() << std::endl;
+	f(arr);
+	std::cout << std::endl;
+
+	std::cout << "FIRST ELEMENT OF ARR:" << std::endl;
+	std::cout << arr << std::endl;
+	std::cout << *arr << std::endl;
+	std::cout << std::endl;
+
+	std::cout << "SECOND ELEMENT OF ARR:" << std::endl;
+	std::cout << arr + 1 << std::endl;
+	std::cout << 1 + arr << std::endl;
+
+	std::cout << arr[1] << std::endl;
+	std::cout << *(arr + 1) << std::endl;
+
+	std::cout << *(1 + arr) << std::endl;
+	std::cout << 1[arr] << std::endl;
+
+	std::cout << std::endl;
+
+	std::cout << "-1 ELEMENT OF ARR:" << std::endl;
+	std::cout << arr[-1] << std::endl;
+	std::cout << *(arr - 1) << std::endl;
+	std::cout << std::endl;
+
+	//std::cout << *(arr + 1) << std::endl;
+	//std::cout << *arr + 1 <<std::endl;
+
+	return 0;
+}
