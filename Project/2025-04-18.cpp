ca9b783e3811d7f94ab74651fbac0513f5ea1349
fursova-au <fursova.a.u@gmail.com>
2025-04-22 17:57:23 +0300

    Код от 22 апреля.

--- Project/2025-04-18.cpp
+++ Project/2025-04-18.cpp
@@ -1,71 +1,71 @@
-#include <iostream>
-#include <vector>
-
-template<typename Type>
-void print(const std::vector<Type>& vector)
-{
-	for (size_t i = 0; i < vector.size(); ++i)
-	{
-		std::cout << vector[i] << " ";
-	}
-	std::cout << std::endl;
-}
-
-void print(int x)
-{
-	std::cout << "INT " << x;
-	std::cout << std::endl;
-}
-
-void print(double x)
-{
-	std::cout << "DOUBLE " << x;
-	std::cout << std::endl;
-}
-
-template <typename T>
-void print(T x)
-{
-	std::cout << "ALL " << x << std::endl;
-}
-
-//тип данных
-struct Point
-{
-	double x;
-	double y;
-};
-
-int main()
-{
-	Point p = { 4, 7 };
-
-	p.x = 5;
-	p.y = 0;
-
-	p = { 7 };
-
-	std::vector<Point> points;
-
-	points.push_back({ 1, 5 });
-
-	std::vector<int> v1 = { 4, 5, 7, 9, -4, 1 };
-
-	print(v1);
-
-	std::vector<double> v2 = { 4.0, 2.5, -3.14, 28.9 };
-
-	print(v2);
-
-	print(4);
-	print(3.14);
-	print(3.14f);
-	print('c');
-	print(5u);
-	print(3.0l);
-
-	print<int>('c');
-	print(static_cast<int>('c'));
-
-	return 0;
-}
+//#include <iostream>
+//#include <vector>
+//
+//template<typename Type>
+//void print(const std::vector<Type>& vector)
+//{
+//	for (size_t i = 0; i < vector.size(); ++i)
+//	{
+//		std::cout << vector[i] << " ";
+//	}
+//	std::cout << std::endl;
+//}
+//
+//void print(int x)
+//{
+//	std::cout << "INT " << x;
+//	std::cout << std::endl;
+//}
+//
+//void print(double x)
+//{
+//	std::cout << "DOUBLE " << x;
+//	std::cout << std::endl;
+//}
+//
+//template <typename T>
+//void print(T x)
+//{
+//	std::cout << "ALL " << x << std::endl;
+//}
+//
+////тип данных
+//struct Point
+//{
+//	double x;
+//	double y;
+//};
+//
+//int main()
+//{
+//	Point p = { 4, 7 };
+//
+//	p.x = 5;
+//	p.y = 0;
+//
+//	p = { 7 };
+//
+//	std::vector<Point> points;
+//
+//	points.push_back({ 1, 5 });
+//
+//	std::vector<int> v1 = { 4, 5, 7, 9, -4, 1 };
+//
+//	print(v1);
+//
+//	std::vector<double> v2 = { 4.0, 2.5, -3.14, 28.9 };
+//
+//	print(v2);
+//
+//	print(4);
+//	print(3.14);
+//	print(3.14f);
+//	print('c');
+//	print(5u);
+//	print(3.0l);
+//
+//	print<int>('c');
+//	print(static_cast<int>('c'));
+//
+//	return 0;
+//}
---
+++ Project/2025-04-22.cpp
@@ -0,0 +1,92 @@
+/*
+
+#include <iostream>
+#include <vector>
+#include <list>
+#include <set>
+//#include <map>
+
+template<typename Type>
+void print(const Type& container)
+{
+	typename Type::const_iterator it = container.begin();
+
+	while (it != container.end())
+	{
+		std::cout << *it << " ";
+		++it;
+	}
+	std::cout << std::endl;
+}
+
+template<typename Iterator>
+void print(Iterator begin, Iterator end)
+{
+	Iterator it = begin;
+
+	while (it != end)
+	{
+		std::cout << *it << " ";
+		++it;
+	}
+	std::cout << std::endl;
+}
+
+int main()
+{
+	std::vector<int> v = { 1, 2, 7, 8, 2, 9 };
+	print(v);
+
+	std::list<int> l = { 1, 2, 7, 8, 2, 9 };
+	print(l);
+
+	std::set<int> s = { 1, 2, 7, 8, 2, 9 };
+	print(s);
+	print(s.begin(), s.end());
+
+	//std::map<std::string, int> m = { {"cat", 1}, {"dog", 2}, {"zebra", 1}};
+
+	//print(l.begin(), l.end());
+
+	return 0;
+}
+
+*/
+
+#include <vector>
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+
+void printElem(int elem)
+{
+	std::cout << std::setw(4) << elem << " ";
+}
+
+bool isLess(int a, int b)
+{
+	if (a % 2 == 0 && b % 2 == 0)
+		return a > b;
+
+	if (a % 2 != 0 && b % 2 != 0)
+		return a < b;
+
+	if (a % 2 == 0)
+		return false;
+
+	//if (b % 2 == 0)
+	return true;
+}
+
+int main()
+{
+	std::vector<int> v = { 1, 14, -7, 2, 7, 8, 2, 9 };
+	std::for_each(v.begin(), v.end(), printElem);
+	std::cout << std::endl;
+
+	std::sort(v.begin(), v.end(), isLess);
+	std::for_each(v.begin(), v.end(), printElem);
+	std::cout << std::endl;
+
+	return 0;
+}
